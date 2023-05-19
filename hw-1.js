4115fcb8de32d5b1ece119941f3497fc22a00b31
Maksim Bersenev <106951162+Bersenev787@users.noreply.github.com>
2023-05-19 08:37:49 +0300

    hw-1
    hw-1

---
+++ hw-1.js
@@ -0,0 +1,101 @@
+/* Задание 1
+
+let a = "10";
+alert(a);
+a = "20";
+alert(a);
+*/
+
+
+
+/* Задание 2
+
+const year = '2007';
+alert(`год выпуска первого айфона ${year}`);
+*/
+
+
+
+/* Задание 3
+
+const nameJs = 'Брендан Эйх';
+alert(`Имя создателя JavaScript ${nameJs}`);
+*/
+
+
+
+/* Задание 4
+
+let a = 10;
+let b = 2;
+alert(a + b);
+alert(a - b);
+alert(a * b);
+alert(a / b);
+*/
+
+
+
+/* Задание 5
+
+let result = 2**5;
+alert(result);
+*/
+
+
+
+/* задание 6
+
+const a = 9;
+const b = 2;
+const result = 9 % 2;
+alert(result);
+*/
+
+
+
+/* Задание 7
+
+let num = 1;
+num += 5;
+num -= 3;
+num *= 7;
+num /=3 ;
+num = ++num;
+num = --num;
+alert(num);
+*/
+
+
+
+/* Задание 8
+
+let age = Number (prompt ('Сколько Вам лет?'))
+alert(age);
+*/
+
+
+
+/* Задание 9
+
+const user = {
+    name: "John",
+    age: 30,
+    isAdmin: false
+};
+user.cityResidence = "New York";
+user.age = 35;
+delete user.city;
+
+const info = prompt("Какую информацию хотите узнать о пользователе?");
+const result = user[info];
+alert(result);
+*/
+
+
+
+/* Задание 10
+
+let newName = prompt ('Как тебя зовут?');
+alert('Привет, ' + newName + '!');
+*/
