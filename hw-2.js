450410c2eb1b702390234191189f30f29da01c58
Maksim Bersenev <106951162+Bersenev787@users.noreply.github.com>
2023-05-25 10:11:24 +0300

    hw-2
    hw-2

---
+++ hw-2.js
@@ -0,0 +1,73 @@
+/*задание 1
+
+let password = 'пароль';
+let answer = prompt('Введите пароль');
+if (password === answer) {
+    alert ('Пароль введен верно');
+} else {
+    alert ('Пароль введен неправильно');
+}
+*/
+
+
+
+/*задание 2
+
+const c = 2;
+if (c > 0 && c < 10 ) {
+    console.log('верно');
+} else {
+    console.log('не верно');
+}
+*/
+
+
+
+/*задание 3
+
+const d = 66;
+const e = 107;
+if (d > 100 || e > 100 ) {
+    console.log('верно');
+} else {
+    console.log('не верно');
+}
+*/
+
+
+
+/*задание 4
+
+let a = '2';
+let b = '3';
+alert (parseInt(a) + parseInt(b));
+*/
+
+
+
+
+
+/*задание 5
+
+function seasons() {
+    let monthNumber = Number(prompt ('Введите месяц в формате числа (от 1 до 12)'));
+
+    switch (true) {
+        case monthNumber >= 3 && UserNum <= 5:
+            alert ('Весна!');
+            break;
+        case monthNumber >= 6 && UserNum <= 8:
+            alert ('Лето!');
+            break;
+        case monthNumber >= 9 && UserNum <= 11:
+            alert ('Осень!');
+            break;
+        case monthNumber === 12 && UserNum >= 2:
+            alert ('Зима!');
+            break;
+        default:
+            alert ('Вы ввели неправильное значение');
+            break;
+    }
+} 
+*/
