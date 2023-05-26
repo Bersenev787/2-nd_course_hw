78d24bf8a98d7247f815c6479bb99be5f5a7fe36
Maksim Bersenev <106951162+Bersenev787@users.noreply.github.com>
2023-05-26 11:43:29 +0300

    Create hw-3.js

---
+++ hw-3.js
@@ -0,0 +1,77 @@
+/* Задание 1
+
+let i = 0;
+while (i < 2) {
+    console.log ('Привет');
+    i++;
+}
+*/
+
+
+
+
+/* Задание 2
+
+let i = 1;
+while (i <= 5) {
+    console.log (i);
+    i++;
+}
+*/
+
+
+
+/* Задание 3
+
+let i = 7;
+while (i <= 22) {
+    console.log (i);
+    i++;
+}
+*/
+
+
+
+
+/* Задание 4
+
+const obj = {
+    Коля: 200, 
+    Вася: 300, 
+    Петя: 400
+};
+
+for (let key in obj) {
+  console.log(`${key} - зарплата ${obj[key]}$.`);
+}
+*/
+
+
+
+
+/* Задание 5
+
+let n = 1000;
+let num = 0;
+
+while (n >= 50) {
+    n /= 2;
+    num++;
+}
+console.log(`Финальное число: ${n}`);
+console.log(`Количество итераций: ${num}`);
+*/
+
+
+
+/* Задание 6
+
+let friday = 2;
+
+do {
+    console.log(`Сегодня пятница ${friday}-е число. Необходимо подготовить отчет.`);
+    friday += 7;
+} 
+while (friday <= 31);
+*/
+
